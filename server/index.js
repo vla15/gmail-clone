f53259fe19794382141070ac7ffa619c927d9d18
Vincent La <vincela15@gmail.com>
2017-08-20 12:58:25 -0700

    serving static files from public directory

--- server/index.js
+++ server/index.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const app = express();
+app.use(express.static(__dirname + '../public/'));
 
-app.get('/', (req, res) => {
-  res.send('hello world');
-})
+
+
+
 
 app.listen(3000, () => {
   console.log('Current listening in on port 3000');
 })
 
+app.get('/', (req, res) => {
+  res.send('hello world');
+})
